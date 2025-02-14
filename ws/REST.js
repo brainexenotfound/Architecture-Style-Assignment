bb27e70356ed56276dd00ad06914d84f3e6b167d
lamisalymni <LamisAlymni@cmu.edu>
2025-02-14 17:09:27 -0500

    add logging for authentication

--- ws/REST.js
+++ ws/REST.js
@@ -28,21 +28,21 @@
 var mysql   = require("mysql");     //Database
 const logger = require("./Logger.js");
 
-function REST_ROUTER(router,connection) {
+function REST_ROUTER(router, connection) {
     var self = this;
     self.sessions = {};
-    self.handleRoutes(router,connection);
+    self.handleRoutes(router, connection);
 }
 
 // Here is where we define the routes. Essentially a route is a path taken through the code dependent upon the 
 // contents of the URL
 
-REST_ROUTER.prototype.handleRoutes= function(router,connection) {
+REST_ROUTER.prototype.handleRoutes = function(router, connection) {
     let self = this;
     // GET with no specifier - returns system version information
     // req paramdter is the request object
     // res parameter is the response object
-
+    
     function generateToken() {
         return (Math.random().toString(36).substr(2) + Date.now().toString(36));
     }
@@ -52,17 +52,19 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         var password = req.body.password;
 
         var SQL = "INSERT INTO accounts (username, password) VALUES (?, ?)";
-        SQL = mysql.format(SQL,[username, password]);
+        SQL = mysql.format(SQL, [username, password]);
         connection.query(SQL, function (err, _) {
             if (err) {
+                logger.logAction(username, "CREATE_ACCOUNT", false);
                 return res.json({ error: true, message: "Error: " + err });
             }
             let token = generateToken();
             self.sessions[token] = username; // Store session
             res.setHeader("token", token);
-            res.json({ error: false, message: "Account created successfully", "token": token });
+            logger.logAction(username, "CREATE_ACCOUNT", true);
+            res.json({ error: false, message: "Account created successfully", token: token });
         });
-    })
+    });
 
     router.get("/login", function(req, res) {
         var username = req.query.username;
@@ -72,6 +74,7 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
         SQL = mysql.format(SQL,[username, password]);
         connection.query(SQL, [username, password], function (err, rows) {
             if (err) {
+                logger.logAction(username, "LOGIN", false);
                 return res.json({ error: true, message: "Error executing MySQL query", sql: SQL });
             }
 
@@ -79,39 +82,44 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
                 let token = generateToken();
                 self.sessions[token] = username; // Store session
                 res.setHeader("token", token);
+                logger.logAction(username, "LOGIN", true);
                 res.json({ error: false, message: "Login successfully", "token": token });
             } else {
+                logger.logAction(username, "LOGIN", false);
                 res.status(401).json({ error: true, message: "Invalid credentials", query: SQL, result: rows });
             }
         });
-    })
+    });
 
-    router.get("/",function(req,res) {
-        res.json({"Message":"Orders Webservices Server Version 1.0"});
+    router.get("/", function(req, res) {
+        let username = req.query.username || "anonymous";
+        logger.logAction(username, "GET_VERSION", true);
+        res.json({ "Message": "Orders Webservices Server Version 1.0" });
     });
     
+    router.get("/orders", function(req, res) {
+
     // GET for /orders specifier - returns all orders currently stored in the database
     // req paramdter is the request object
     // res parameter is the response object
   
-    router.get("/orders",function(req,res){
-        const username = req.query.username || "anonymous";
-        let token = req.headers.token;
+    let token = req.headers.token;
         if (!token || !self.sessions[token]) {
+            logger.logAction("anonymous", "UNAUTHORIZED GET_ALL_ORDERS", false);
             return res.status(401).json({ error: true, message: "Unauthorized" });
         }
-
-        console.log("Getting all database entries..." );
+        const username = self.sessions[token];
+        console.log("Getting all database entries...");
         var query = "SELECT * FROM ??";
         var table = ["orders"];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if (err) {
                 logger.logAction(username, "GET_ALL_ORDERS", false);
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
                 logger.logAction(username, "GET_ALL_ORDERS", true);
-                res.json({"Error" : false, "Message" : "Success", "Orders" : rows});
+                res.json({ "Error": false, "Message": "Success", "Orders": rows });
             }
         });
     });
@@ -119,26 +127,25 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // GET for /orders/order id specifier - returns the order for the provided order ID
     // req paramdter is the request object
     // res parameter is the response object
-     
-    router.get("/orders/:order_id",function(req,res){
-        const username = req.query.username || "anonymous";
+
+    router.get("/orders/:order_id", function(req, res) {
         let token = req.headers.token;
         if (!token || !self.sessions[token]) {
-            logger.logAction(username, "UNAUTHORIZED GET_ORDER_BY_ID", false);
+            logger.logAction("anonymous", "UNAUTHORIZED GET_ORDER_BY_ID", false);
             return res.status(401).json({ error: true, message: "Unauthorized" });
         }
-
-        console.log("Getting order ID: ", req.params.order_id );
+        const username = self.sessions[token];
+        console.log("Getting order ID: ", req.params.order_id);
         var query = "SELECT * FROM ?? WHERE ??=?";
-        var table = ["orders","order_id",req.params.order_id];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
+        var table = ["orders", "order_id", req.params.order_id];
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if (err) {
                 logger.logAction(username, "GET_ORDER_BY_ID", false);
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
                 logger.logAction(username, "GET_ORDER_BY_ID", true);
-                res.json({"Error" : false, "Message" : "Success", "Users" : rows});
+                res.json({ "Error": false, "Message": "Success", "Users": rows });
             }
         });
     });
@@ -146,49 +153,48 @@ REST_ROUTER.prototype.handleRoutes= function(router,connection) {
     // POST for /orders?order_date&first_name&last_name&address&phone - adds order
     // req paramdter is the request object - note to get parameters (eg. stuff afer the '?') you must use req.body.param
     // res parameter is the response object 
-  
-    router.post("/orders",function(req,res){
-        const username = req.query.username || "anonymous";
+
+    router.post("/orders", function(req, res) {
         let token = req.headers.token;
         if (!token || !self.sessions[token]) {
-            logger.logAction(username, "UNAUTHORIZED CREATE_ORDER", false);
+            logger.logAction("anonymous", "UNAUTHORIZED CREATE_ORDER", false);
             return res.status(401).json({ error: true, message: "Unauthorized" });
         }
-
-        //console.log("url:", req.url);
-        //console.log("body:", req.body);
-        console.log("Adding to orders table ", req.body.order_date,",",req.body.first_name,",",req.body.last_name,",",req.body.address,",",req.body.phone);
+        const username = self.sessions[token];
+        console.log("Adding to orders table ", req.body.order_date, ",", req.body.first_name, ",", req.body.last_name, ",", req.body.address, ",", req.body.phone);
         var query = "INSERT INTO ??(??,??,??,??,??) VALUES (?,?,?,?,?)";
-        var table = ["orders","order_date","first_name","last_name","address","phone",req.body.order_date,req.body.first_name,req.body.last_name,req.body.address,req.body.phone];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
+        var table = ["orders", "order_date", "first_name", "last_name", "address", "phone",
+                     req.body.order_date, req.body.first_name, req.body.last_name, req.body.address, req.body.phone];
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if (err) {
                 logger.logAction(username, "CREATE_ORDER", false);
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
                 logger.logAction(username, "CREATE_ORDER", true);
-                res.json({"Error" : false, "Message" : "User Added !"});
+                res.json({ "Error": false, "Message": "User Added !" });
             }
         });
     });
 
-    router.delete("/orders/:order_id",function(req,res){
+    router.delete("/orders/:order_id", function(req, res) {
         let token = req.headers.token;
         if (!token || !self.sessions[token]) {
-            logger.logAction(username, "UNAUTHORIZED DELETE_ORDER", false);
+            logger.logAction("anonymous", "UNAUTHORIZED DELETE_ORDER", false);
             return res.status(401).json({ error: true, message: "Unauthorized" });
         }
-        console.log("Deleting order ID: ", req.params.order_id );
+        const username = self.sessions[token];
+        console.log("Deleting order ID: ", req.params.order_id);
         var query = "DELETE FROM ?? WHERE ??=?";
-        var table = ["orders","order_id",req.params.order_id];
-        query = mysql.format(query,table);
-        connection.query(query,function(err,rows){
-            if(err) {
+        var table = ["orders", "order_id", req.params.order_id];
+        query = mysql.format(query, table);
+        connection.query(query, function(err, rows) {
+            if (err) {
                 logger.logAction(username, "DELETE_ORDER", false);
-                res.json({"Error" : true, "Message" : "Error executing MySQL query"});
+                res.json({ "Error": true, "Message": "Error executing MySQL query" });
             } else {
                 logger.logAction(username, "DELETE_ORDER", true);
-                res.json({"Error" : false, "Message" : "Success"});
+                res.json({ "Error": false, "Message": "Success" });
             }
         });
     });
